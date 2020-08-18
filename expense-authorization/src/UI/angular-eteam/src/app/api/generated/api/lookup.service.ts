86ff52cd5fd3eb124e4273b77a734bf08e7d7ca4
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2020-08-18 15:14:34 -0700

    UI: regenerated API, added new resourceType lookup to screen

--- expense-authorization/src/UI/angular-eteam/src/app/api/generated/api/lookup.service.ts
+++ expense-authorization/src/UI/angular-eteam/src/app/api/generated/api/lookup.service.ts
@@ -88,6 +88,47 @@ export class LookupService {
     }
 
     /**
+     * Gets the asynchronous.
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public apiLookupExpenditureAuthorizationResourceTypesGet(observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json'}): Observable<Array<LookupValue>>;
+    public apiLookupExpenditureAuthorizationResourceTypesGet(observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json'}): Observable<HttpResponse<Array<LookupValue>>>;
+    public apiLookupExpenditureAuthorizationResourceTypesGet(observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json'}): Observable<HttpEvent<Array<LookupValue>>>;
+    public apiLookupExpenditureAuthorizationResourceTypesGet(observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json'}): Observable<any> {
+
+        let headers = this.defaultHeaders;
+
+        let httpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
+        if (httpHeaderAcceptSelected === undefined) {
+            // to determine the Accept header
+            const httpHeaderAccepts: string[] = [
+                'application/json'
+            ];
+            httpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        }
+        if (httpHeaderAcceptSelected !== undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+
+        let responseType: 'text' | 'json' = 'json';
+        if(httpHeaderAcceptSelected && httpHeaderAcceptSelected.startsWith('text')) {
+            responseType = 'text';
+        }
+
+        return this.httpClient.get<Array<LookupValue>>(`${this.configuration.basePath}/api/Lookup/ExpenditureAuthorizationResourceTypes`,
+            {
+                responseType: <any>responseType,
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
      * @param lookupType 
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
      * @param reportProgress flag to report request and response progress.
