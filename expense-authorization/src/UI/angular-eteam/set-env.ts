95bd11aaea1e191afd3a6d7534e35eafac9a7b7d
Alan Dodge <alan.dodge@gov.bc.ca>
2020-09-02 19:28:51 -0700

    Removing unneeded console logs, comments, etc

--- expense-authorization/src/UI/angular-eteam/set-env.ts
+++ expense-authorization/src/UI/angular-eteam/set-env.ts
@@ -1,22 +1,17 @@
-// import { writeFile } from 'fs';
 const fs = require("fs");
-// Configure Angular `environment.ts` file path
 const targetPath = './src/environments/environment.prod.ts';
-// Load node modules
 const dotenv = require('dotenv').config();
 
-// `environment.ts` file structure
 const envConfigFile = `export const environment = {
   production: true,
   apiEndpoint: '${process.env.API_URL}'
 };
 `;
-console.log('The file `environment.ts` will be written with the following content: \n');
-console.log(envConfigFile);
+
 fs.writeFile(targetPath, envConfigFile, function (err) {
   if (err) {
     throw console.error(err);
   } else {
-    console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+    console.log(`Angular environment.prod.ts file generated correctly at ${targetPath} \n`);
   }
 });
